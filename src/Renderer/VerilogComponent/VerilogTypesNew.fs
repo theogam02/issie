a6eca4a73cc9641645abe2b32ae0fc9e53947d2a
petraratkai <pr2518@imperial.ac.uk>
2023-02-17 16:24:02 +0000

    Add foldAST function

--- src/Renderer/VerilogComponent/VerilogTypesNew.fs
+++ src/Renderer/VerilogComponent/VerilogTypesNew.fs
@@ -8,7 +8,7 @@ open VerilogTypes
 // Only one module in one file?
 
 
-type Item =
+type ItemDU =
     | IOItem of IOItemT
     | ParamDecl of ParameterItemT
     | ContinuousAssign of ContinuousAssignT
@@ -28,9 +28,13 @@ type ASTNode =
     | ContinuousAssign of ContinuousAssignT
     | AlwaysConstruct of AlwaysConstructT
     | Statement of StatementT
-    // | NonBlockingAssign of NonBlockingAssignT
-    // | BlockingAssign of BlockingAssignT
-    // | SeqBlock of SeqBlockT
+    | NonBlockingAssign of NonBlockingAssignT
+    | BlockingAssign of BlockingAssignT
+    | SeqBlock of SeqBlockT
+    | Case of CaseStatementT
+    | CaseItem of CaseItemT
+    | Conditional of ConditionalT
+    | IfStatement of IfStatementT
     | Assignment of AssignmentT
     | AssignmentLHS of AssignmentLHST
     | Expression of ExpressionT
@@ -56,6 +60,60 @@ let getAlwaysStatement (s: StatementT) : StatementDU =
     | None, None, None, Some cond, None -> StatementDU.Conditional(cond)
     | None, None, None, None, Some seqBlock -> StatementDU.SeqBlock(seqBlock)
     | _ -> failwithf "Should not happen!"
+//maybe we can combine these two, or do smth smarter
+let statementToNode (statement:StatementDU) : ASTNode =
+    match statement with
+    | StatementDU.BlockingAssign blocking -> BlockingAssign(blocking)
+    | StatementDU.NonBlockingAssign nonblocking -> NonBlockingAssign(nonblocking)
+    | StatementDU.Case case -> Case(case)
+    | StatementDU.Conditional cond -> Conditional(cond)
+    | StatementDU.SeqBlock seqBlock -> SeqBlock(seqBlock)
+
+
+let getItem (item: ItemT)  =
+    match item.IODecl, item.ParamDecl, item.Statement, item.AlwaysConstruct with
+    | Some ioDecl, None, None, None -> IOItem ioDecl
+    | None, Some paramDecl, None, None -> ParamDecl paramDecl
+    | None, None, Some contAssign, None -> ContinuousAssign contAssign
+    | None, None, None, Some always -> AlwaysConstruct always
+    | _ -> failwithf "Should not happen"
+
+/// Recursively folds over an ASTNode, calling folder at every level. Only explores parts where there are multiple possibilities within a Node
+let rec foldAST folder state (node:ASTNode) =
+    let state' = folder state node
+    match node with
+    | Item item -> 
+        foldAST folder state' (getItem item)
+    | AlwaysConstruct always ->
+        foldAST folder state' (Statement(always.Statement))
+    | Statement statement -> 
+        statement
+        |> getAlwaysStatement
+        |> statementToNode
+        |> foldAST folder state'
+    | SeqBlock seqBlock ->
+        seqBlock.Statements
+        |> Array.map (fun s -> Statement(s))
+        |> Array.fold (foldAST folder) state'
+    | Case case ->
+        let tmpState = 
+            case.CaseItems
+            |> Array.map (fun item -> CaseItem(item))
+            |> Array.fold (foldAST folder) state'
+        match case.Default with
+        | Some stmt -> List.fold folder tmpState [Statement(stmt)]
+        | _ -> tmpState
+    | CaseItem caseItem -> foldAST (foldAST folder) state' (Statement(caseItem.Statement))
+    | Conditional cond ->
+        let tmpState =
+            cond.IfStatements
+            |> Array.map (fun stmt -> IfStatement(stmt))
+            |> Array.fold (foldAST folder) state'
+        match cond.ElseStatement with
+        | Some elseStmt -> List.fold (foldAST folder) tmpState [Statement(elseStmt)]
+        | _ -> tmpState
+    | _ -> failwithf "Should not happen"
+
 
 /// returns a list of all assignments in the AST
 let rec getAllAssignments (assignments) (node: ASTNode) =
@@ -67,13 +125,13 @@ let rec getAllAssignments (assignments) (node: ASTNode) =
         | _ -> assignments
     | Statement statement ->
         match getAlwaysStatement statement with
-        | BlockingAssign blocking -> assignments @ [blocking.Assignment]
-        | NonBlockingAssign nonblocking -> assignments @ [nonblocking.Assignment]
-        | SeqBlock seqBlock -> 
+        | StatementDU.BlockingAssign blocking -> assignments @ [blocking.Assignment]
+        | StatementDU.NonBlockingAssign nonblocking -> assignments @ [nonblocking.Assignment]
+        | StatementDU.SeqBlock seqBlock -> 
             seqBlock.Statements
             |> Array.map (fun s -> Statement(s))
             |> Array.fold getAllAssignments assignments
-        | Conditional cond ->
+        | StatementDU.Conditional cond ->
             let ifAssignments =
                 cond.IfStatements
                 |> Array.map (fun ifstmt -> Statement(ifstmt.Statement))
@@ -82,7 +140,7 @@ let rec getAllAssignments (assignments) (node: ASTNode) =
             | Some statement -> 
                 getAllAssignments ifAssignments (Statement(statement))
             | _ -> ifAssignments
-        | Case case -> 
+        | StatementDU.Case case -> 
             let caseAssignments =
                 case.CaseItems
                 |> Array.map (fun caseitem -> Statement(caseitem.Statement))
@@ -103,11 +161,11 @@ let rec getAllExpressions (expressions: List<ExpressionT>) (node: ASTNode) =
         | _ -> expressions
     | Statement s ->
         match getAlwaysStatement s with
-        | SeqBlock seqBlock -> 
+        | StatementDU.SeqBlock seqBlock -> 
             seqBlock.Statements
             |> Array.map (fun s -> Statement(s))
             |> Array.fold getAllExpressions expressions
-        | Conditional cond ->
+        | StatementDU.Conditional cond ->
             let ifConditions =
                 cond.IfStatements
                 |> Array.toList
@@ -122,7 +180,7 @@ let rec getAllExpressions (expressions: List<ExpressionT>) (node: ASTNode) =
             | Some statement -> 
                 getAllExpressions expr (Statement(statement))
             | _ -> expr
-        | Case case ->
+        | StatementDU.Case case ->
             let caseItemExpr =
                 case.CaseItems
                 |> Array.collect (fun caseItem -> caseItem.Expressions)
